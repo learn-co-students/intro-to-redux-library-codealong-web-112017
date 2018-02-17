6e41310103565f1491293e54065d078f1b931b60
Marvin Clerge <clergemarvin@gmail.com>
2018-02-17 01:56:25 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import shoppingListItemReducer from './reducers/shoppingListItemReducer';
 import App from './App';
 import './index.css';
+import { createStore } from 'redux'
+
+let store = createStore(shoppingListItemReducer)
 
 ReactDOM.render(
-  <App />,
+  <App store={store}/>,
   document.getElementById('root')
 );
--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -4,9 +4,12 @@ export default function shoppingListItemReducer(state = {
   switch(action.type) {
 
     case 'INCREASE_COUNT':
+      console.log('previous',state.items.length);
+      console.log('next',state.items.length + 1);
       return Object.assign({}, state, { items: state.items.concat(state.items.length + 1) });
 
     default:
+    console.log('initial', state.items.length);
       return state;
   }
 }
